4657af1ce9f9aa75f8c41a9e93c13eeaa4b623be
KamToHung <478409784qq@gmail.com>
2022-07-12 18:05:14 +0800

    feat: add too numbers

---
+++ 算法/2. 两数相加/AddTwoNumbers.go
@@ -0,0 +1,10 @@
+package ___两数相加
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return l1
+}
---
+++ 算法/2. 两数相加/AddTwoNumbers.java
@@ -0,0 +1,49 @@
+
+public class AddTwoNumbers {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode node = new ListNode();
+            ListNode cur = node;
+            int flag = 0;
+            while (l1 != null || l2 != null) {
+                int m = l1 == null ? 0 : l1.val;
+                int n = l2 == null ? 0 : l2.val;
+                int sum = m + n + flag;
+                flag = sum / 10;
+                int nowNum = sum % 10;
+                cur.next = new ListNode(nowNum);
+                cur = cur.next;
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
+            }
+            if (flag == 1) {
+                cur.next = new ListNode(1);
+            }
+            return node.next;
+        }
+    }
+
+}
