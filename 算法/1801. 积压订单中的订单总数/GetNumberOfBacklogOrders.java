ad12576dcb7862686bdc18fa899159e686aa3280
kamtohung <kamtohung@gmail.com>
2023-01-02 23:09:39 +0800

    feat: 1081

---
+++ 算法/1801. 积压订单中的订单总数/GetNumberOfBacklogOrders.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode.cn/problems/number-of-orders-in-the-backlog/solutions/2039856/ji-ya-ding-dan-zhong-de-ding-dan-zong-sh-6g22/
+ */
+class Solution {
+    public int getNumberOfBacklogOrders(int[][] orders) {
+        final int MOD = 1000000007;
+        PriorityQueue<int[]> buyOrders = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        PriorityQueue<int[]> sellOrders = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int[] order : orders) {
+            int price = order[0], amount = order[1], orderType = order[2];
+            if (orderType == 0) {
+                while (amount > 0 && !sellOrders.isEmpty() && sellOrders.peek()[0] <= price) {
+                    int[] sellOrder = sellOrders.poll();
+                    int sellAmount = Math.min(amount, sellOrder[1]);
+                    amount -= sellAmount;
+                    sellOrder[1] -= sellAmount;
+                    if (sellOrder[1] > 0) {
+                        sellOrders.offer(sellOrder);
+                    }
+                }
+                if (amount > 0) {
+                    buyOrders.offer(new int[]{price, amount});
+                }
+            } else {
+                while (amount > 0 && !buyOrders.isEmpty() && buyOrders.peek()[0] >= price) {
+                    int[] buyOrder = buyOrders.poll();
+                    int buyAmount = Math.min(amount, buyOrder[1]);
+                    amount -= buyAmount;
+                    buyOrder[1] -= buyAmount;
+                    if (buyOrder[1] > 0) {
+                        buyOrders.offer(buyOrder);
+                    }
+                }
+                if (amount > 0) {
+                    sellOrders.offer(new int[]{price, amount});
+                }
+            }
+        }
+        int total = 0;
+        for (PriorityQueue<int[]> pq : Arrays.asList(buyOrders, sellOrders)) {
+            while (!pq.isEmpty()) {
+                int[] order = pq.poll();
+                total = (total + order[1]) % MOD;
+            }
+        }
+        return total;
+    }
+
+}
