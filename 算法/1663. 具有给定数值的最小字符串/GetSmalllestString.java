a64a34392984ed9a221e7efb5056720a20b0172c
kamtohung <kamtohung@gmail.com>
2023-01-26 15:05:06 +0800

    feat: 1663 先补全再填充

--- 算法/1663. 具有给定数值的最小字符串/GetSmalllestString.java
+++ 算法/1663. 具有给定数值的最小字符串/GetSmalllestString.java
@@ -4,7 +4,7 @@ public class GetSmalllestString {
 
     class Solution {
 
-        public String getSmalllestString(int n, int k) {
+        public String getSmallestString(int n, int k) {
             // 首先先把n个位置都填充a，然后从最后一个位置往前一个个填充即可
             char[] chs = new char[n];
             // 填充a
@@ -13,13 +13,16 @@ public class GetSmalllestString {
             k = k - 1 * n;
             int pos = 1;
             // 往后填充
-            // 也可以优化为批量处理，判断k的值是否大于等于25(因为本来初始化已经扣掉1(a)了)
             while (k != 0) {
-                chs[n - pos]++;
-                k--;
-                // 如果等于z了往前移
-                if (chs[n - pos] == 'z') {
+                // 如果k大于等于25，即为'z'，则替换成z然后k-25
+                if (k >= 25) {
+                    k = k - 25;
+                    chs[n - pos] += 25;
                     pos++;
+                } else {
+                    // 这时候k不足'z'，直接取剩余的数即可
+                    chs[n - pos] += k;
+                    break;
                 }
             }
             return String.valueOf(chs);
