9d8a7b7838c038aa5514c11a63b24482060c8892
kamtohung <kamtohung@gmail.com>
2023-02-03 00:38:46 +0800

    init 1129

---
+++ 算法/1129. 颜色交替的最短路径/ShortestAlternatingPaths.java
@@ -0,0 +1,13 @@
+/**
+ * @author <a href = "kamtohung@gmail.com">KamTo Hung</a>
+ */
+public class ShortestAlternatingPaths {
+
+    class Solution {
+        public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+            return null;
+        }
+
+    }
+
+}
