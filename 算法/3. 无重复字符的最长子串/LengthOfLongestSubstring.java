ee64a5abf5508edc54d2cc5034fb7389240ba3cf
kamtohung <kamtohung@gmail.com>
2023-02-16 17:17:37 +0800

    feat: 3

--- 算法/3. 无重复字符的最长子串/LengthOfLongestSubstring.java
+++ 算法/3. 无重复字符的最长子串/LengthOfLongestSubstring.java
@@ -9,24 +9,24 @@ public class LengthOfLongestSubstring {
                 return 0;
             }
             Map<Character, Integer> memo = new HashMap<>();
+            int result = 0;
             int left = 0;
             int right = 0;
-            int max = 1;
-            char[] chs = s.toCharArray();
-            while (right < s.length()) {
-                char value = chs[right];
-                int count = memo.getOrDefault(value, 0);
-                memo.put(value, count + 1);
+            // 循环查询最长不重复字符串
+            for (int i = 0; i < s.length(); i++) {
+                char value = s.charAt(i);
+                memo.put(value, memo.getOrDefault(value, 0) + 1);
+                // 判断是否有重复，有的话left到不重复为止
                 while (memo.get(value) > 1) {
-                    char leftValue = chs[left];
-                    int leftCount = memo.getOrDefault(leftValue, 0);
-                    memo.put(leftValue, leftCount - 1);
+                    char leftValue = s.charAt(left);
+                    // 计数扣减
+                    memo.put(leftValue, memo.getOrDefault(leftValue, 0) - 1);
                     left++;
                 }
-                max = Math.max(max, right - left + 1);
+                result = Math.max(result, right - left + 1);
                 right++;
             }
-            return max;
+            return result;
         }
     }
 
