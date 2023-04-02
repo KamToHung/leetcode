1fee2fa55df0875d0cb138f1d580190c5602a0f5
kamtohung <kamtohung@gmail.com>
2023-04-03 01:57:00 +0800

    feat: 1053

--- 算法/1053. 交换一次的先前排列/PrevPermOpt1.java
+++ 算法/1053. 交换一次的先前排列/PrevPermOpt1.java
@@ -2,4 +2,28 @@
  * @author <a href = "kamtohung@gmail.com">KamTo Hung</a>
  */
 public class PrevPermOpt1 {
+
+    class Solution {
+        public int[] prevPermOpt1(int[] arr) {
+            int n = arr.length;
+            for (int i = n - 1 ; i > 0 ; i--) {
+                if (arr[i - 1] > arr[i]) {
+                    int maxIdx = i;
+                    for (int j = i ; j < n ; j++) {
+                        if (arr[i - 1] > arr[j] && arr[maxIdx] < arr[j])
+                            maxIdx = j;
+                    }
+                    swap(arr, i - 1, maxIdx);
+                    break;
+                }
+            }
+            return arr;
+        }
+        void swap(int[] arr, int i, int j) {
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+    }
+
 }
