f95db4902538bec30071c185fbb9997f43b3d95c
Terry <478409784@qq.com>
2023-05-19 16:21:11 +0800

    变更

--- 算法/2. 两数相加/AddTwoNumbers.java
+++ 算法/2. 两数相加/AddTwoNumbers.java
@@ -20,7 +20,7 @@ public class AddTwoNumbers {
     }
 
     class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
             ListNode node = new ListNode();
             ListNode cur = node;
             int flag = 0;
@@ -43,6 +43,31 @@ public class AddTwoNumbers {
                 cur.next = new ListNode(1);
             }
             return node.next;
+        }
+
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode result = new ListNode();
+            ListNode cur = result;
+            int flag = 0;
+            while (l1 != null || l2 != null) {
+                int m = l1 == null ? 0 : l1.val;
+                int n = l2 == null ? 0 : l2.val;
+                int sum = m + n + flag;
+                int curValue = sum % 10;
+                flag = sum / 10;
+                cur.next = new ListNode(curValue);
+                cur = cur.next;
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
+            }
+            if (flag != 0) {
+                cur.next = new ListNode(flag);
+            }
+            return result.next;
         }
     }
 
