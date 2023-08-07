2ce03b95aee251798e4497c219ec85f6a4501864
KamToHung <kamtohung@gmail.com>
2023-08-07 11:32:29 +0800

    feat(golang): 2

--- 算法/2. 两数相加/AddTwoNumbers.go
+++ 算法/2. 两数相加/AddTwoNumbers.go
@@ -6,5 +6,38 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return l1
+	node := &ListNode{}
+	cur := node
+	flag := 0
+	for l1 != nil || l2 != nil {
+		var m = 0
+		var n = 0
+		if l1 != nil {
+			m = l1.Val
+		}
+		if l2 != nil {
+			n = l2.Val
+		}
+		sum := m + n + flag
+		flag = sum / 10
+		curValue := sum % 10
+		curNode := &ListNode{
+			Val: curValue,
+		}
+		cur.Next = curNode
+		cur = cur.Next
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+	// 最后如果还有进位未处理需要加一
+	if flag == 1 {
+		cur.Next = &ListNode{
+			Val: flag,
+		}
+	}
+	return node.Next
 }
